8efd0c8a88279ecdb614a3a82a471f62182e1396
Curia Adrian <adrian.curia@metrosystems.net>
2017-07-26 10:19:27 +0300

    first commit - only spring functionaly, contains already implemented pagination on repo

---
+++ src/main/java/com/dbbyte/SbMysqlPaginationApplication.java
@@ -0,0 +1,12 @@
+package com.dbbyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMysqlPaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMysqlPaginationApplication.class, args);
+	}
+}
---
+++ src/main/java/com/dbbyte/repository/BookRepository.java
@@ -0,0 +1,25 @@
+package com.dbbyte.repository;
+
+
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import com.dbbyte.domain.Book;
+
+public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
+    
+    public Book findByIsbn(@Param("isbn") String isbn);
+    
+    @RestResource(path="getBooksByAuthor")
+    public List<Book> findByAuthorIgnoreCaseOrderByTitleAsc(@Param("author") String author);  
+    
+    @Query("SELECT b FROM Book b WHERE b.published BETWEEN :startYear AND :endYear ORDER BY b.published")
+    public List<Book> getBooksBetweenYears(@Param("startYear")int startYear, @Param("endYear")int endYear);
+    
+    public List<Book> findByTitleContaining(@Param("title") String title); 
+   
+    public int countByAuthor(@Param("author") String author);
+} 
